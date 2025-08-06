72f3fb6305b91e44fdd25018bfd13984b118948f
idkname471 <hassanai.tks2555@gmail.com>
2025-08-06 16:34:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function sayHi() {
   const random = messages[Math.floor(Math.random() * messages.length)];
   document.getElementById("output").innerText = random;
 }
+
+// Rickroll redirect button
+document.getElementById("rickroll-button").addEventListener("click", function() {
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+});
